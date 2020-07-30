01539162b0964848ca552e0c55fddf0a6b4b7233
subithiru07 <subi.07.thiru@gmail.com>
2020-07-30 18:45:49 +0530

    Assignment2

---
+++ THBS/src/THBS.java
@@ -0,0 +1,30 @@
+
+public class THBS {
+
+}
+class Team{
+	WickwtKkepper Wk;
+	BatsMan Bm;
+	Bowler B;
+	AllRounder Ar;
+	
+	
+ }
+class Men extends Team{
+	
+}
+class Women extends Team{
+	
+}
+class WickwtKkepper{
+	
+}
+class BatsMan{
+	
+}
+class Bowler{
+	
+}
+class AllRounder{
+	
+}
